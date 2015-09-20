dac6ce2ad6492667eedf175f378feb0fdc09868e
midoribito <jingaimidoribito@gmail.com>
2015-09-21 02:45:52 +0900

    basic configuration

--- gulpfile.js
+++ gulpfile.js
@@ -12,16 +12,16 @@ var prettify = require('gulp-prettify');
 var htmlPrettify = require('gulp-html-prettify');
 var htmlhint = require('gulp-htmlhint');
 var sass = require('gulp-sass');
-var filter = require('gulp-filter');
-var autoprefixer = require('gulp-autoprefixer');
+// var filter = require('gulp-filter');
+// var autoprefixer = require('gulp-autoprefixer');
 var eventStream = require('event-stream').merge;
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
-var wiredep = require('wiredep').stream;
-var useref = require('gulp-useref'),
-    assets = useref.assets({searchPath: 'dist'});
-var rename = require('gulp-rename');
+// var wiredep = require('wiredep').stream;
+// var useref = require('gulp-useref'),
+    // assets = useref.assets({searchPath: 'dist'});
+// var rename = require('gulp-rename');
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync'),
     reload = browserSync.reload;
@@ -46,25 +46,23 @@ var watch = require('gulp-watch');
 */
 
 gulp.task('ejs', function() {
-  gulp.src(['app/src/modules/**/*.ejs'])
+  gulp.src(['app/src/modules/view/*.ejs'])
   .pipe(ejs())
   .pipe(prettify())
   .pipe(htmlPrettify({
     indent_size: 2
   }))
   .pipe(htmlhint())
-  .pipe(gulp.dest('dist'))
+  .pipe(gulp.dest('dist/view'))
 });
 
 gulp.task('sass', function(done) {
-  gulp.src(['app/src/sass/main.scss'])
+  gulp.src(['app/src/sass/*.scss'])
   .pipe(plumber())
   .pipe(sass())
-  .pipe(filter('sass/main.css'))
-  .pipe(autoprefixer())
-  .pipe(gulp.dest('dist/css'))
+  .pipe(gulp.dest('app/src/css'))
   .on('end', function() {
-    gulp.src(['bower_components/normalize-css/normalize.css', 'dist/css/*.css'])
+    gulp.src(['bower_components/normalize-css/normalize.css', 'app/src/css/*.css'])
     .pipe(concat('main.css'))
     .pipe(gulp.dest('dist/css'))
     .on('end', done);
@@ -80,14 +78,14 @@ gulp.task('js', function() {
     }))
     .pipe(gulp.dest('dist/js')),
 
-    gulp.src(['bower_components/bootstrap-sass/assets/javascripts/bootstrap/*js'])
+    gulp.src(['bower_components/bootstrap-sass/assets/javascripts/bootstrap/*.js'])
     .pipe(concat('plugins.js'))
     .pipe(uglify({
       preserveComments: 'some'
     }))
     .pipe(gulp.dest('dist/js')),
 
-    gulp.src(['app/src/js/*js'])
+    gulp.src(['app/src/js/*.js'])
     .pipe(concat('main.js'))
     .pipe(uglify({
       preserveComments: 'some'
@@ -96,19 +94,7 @@ gulp.task('js', function() {
   )
 });
 
-gulp.task('wiredep', function () {
-  gulp.src('dist/*.html')
-    .pipe(wiredep({
-      exclude: ['bootstrap-sass'],
-      ignorePath: /^(\.\.\/)*\.\./
-    }))
-    .pipe(assets)
-    .pipe(assets.restore())
-    .pipe(useref())
-    .pipe(gulp.dest('dist'))
-});
-
-gulp.task('serve', function () {
+gulp.task('serve', ['build'], function () {
   browserSync({
     notify: false,
     port: 9000,
@@ -121,19 +107,19 @@ gulp.task('serve', function () {
   });
 
   gulp.watch([
-    'dist/**/*.html',
+    'dist/view/**/*.html',
     'dist/css/*css',
     'dist/js/*js'
   ]).on('change', reload);
 
   gulp.watch(['app/src/common/*.ejs'], ['ejs']);
-  gulp.watch(['app/src/modules/**/*.ejs'], ['ejs']);
+  gulp.watch(['app/src/modules/view/**/*.ejs'], ['ejs']);
   gulp.watch(['app/src/sass/*.scss'], ['sass']);
   gulp.watch(['app/src/js/*.js'], ['js']);
 });
 
 gulp.task('build', function() {
-  runSequence('ejs', 'sass', 'js', 'wiredep');
+  runSequence('ejs', 'sass', 'js');
 });
 
 gulp.task('default', function() {
